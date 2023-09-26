472274512d8b72b5de165439514c4cac3134498f
usuario <usuario@pc21-091>
2023-09-26 17:05:48 +0200

    se agrego otro print en el main

--- src/tuto/HolaMunddo.java
+++ src/tuto/HolaMunddo.java
@@ -5,6 +5,7 @@ public class HolaMunddo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola aaaaaa");
+		System.out.println("bienvenida al curso de calidad");
 	}
 
 }
